e05bd6be4e06ea0815be0d102424d2ed5fbbd8a1
smrutikapatil <smruti.patil17@gmail.com>
2022-02-09 21:44:20 +0530

    Added- Calculative gambler won or lost 50% stake

--- src/com/gambler_problem/GamblerProblem.java
+++ src/com/gambler_problem/GamblerProblem.java
@@ -4,15 +4,22 @@ public class GamblerProblem {
 	public static void main(String[] args) {
 		int startingTotalAmount = 100;
 		int betsPerGame = 1;
-		double winOrLoose = Math.floor(Math.random() * 10) % 2;
-		if (betsPerGame == winOrLoose) {
-			System.out.println("you won the game");
-			startingTotalAmount = startingTotalAmount + 1;
-			System.out.println("After winning the game your total amount is:" + startingTotalAmount);
-		} else {
-			System.out.println("you loose the game");
-			startingTotalAmount = startingTotalAmount - 1;
-			System.out.println("After loosing the game your total amount is:" + startingTotalAmount);
+		while (startingTotalAmount > 50 && startingTotalAmount < 150) {
+			double winOrLoose = Math.floor(Math.random() * 10) % 2;
+			if (betsPerGame == winOrLoose) {
+				System.out.println("you won the game");
+				startingTotalAmount = startingTotalAmount + 1;
+				System.out.println("After winning the game your total amount is:" + startingTotalAmount);
+			} else {
+				System.out.println("you loose the game");
+				startingTotalAmount = startingTotalAmount - 1;
+				System.out.println("After loosing the game your total amount is:" + startingTotalAmount);
+			}
+			if (startingTotalAmount == 50) {
+				System.out.println("Today you loose your 50% amount so you have to resign for the day");
+			} else {
+				System.out.println("Today you won your 50% amount so you have to resign for the day");
+			}
 		}
 	}
 }
