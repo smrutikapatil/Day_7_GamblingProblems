0e8bef6918871a2194201a11a111dbe016c40b14
smrutikapatil <smruti.patil17@gmail.com>
2022-02-09 21:53:57 +0530

    Added- Total amount after 20 days

--- src/com/gambler_problem/GamblerProblem.java
+++ src/com/gambler_problem/GamblerProblem.java
@@ -1,25 +1,43 @@
 package com.gambler_problem;
 
 public class GamblerProblem {
+	
 	public static void main(String[] args) {
-		int startingTotalAmount = 100;
+		int stake = 100;
 		int betsPerGame = 1;
-		while (startingTotalAmount > 50 && startingTotalAmount < 150) {
-			double winOrLoose = Math.floor(Math.random() * 10) % 2;
-			if (betsPerGame == winOrLoose) {
-				System.out.println("you won the game");
-				startingTotalAmount = startingTotalAmount + 1;
-				System.out.println("After winning the game your total amount is:" + startingTotalAmount);
-			} else {
-				System.out.println("you loose the game");
-				startingTotalAmount = startingTotalAmount - 1;
-				System.out.println("After loosing the game your total amount is:" + startingTotalAmount);
+		int totalAmount = 0;
+		for (int i = 1; i <= 20; i++) {
+			while (stake > 50 && stake < 150) {
+				double winOrLoose = Math.floor(Math.random() * 10) % 2;
+				if (betsPerGame == winOrLoose) {
+					System.out.println("you won the game");
+					stake = stake + 1;
+					System.out.println("After winning the game your total amount is:" + stake);
+				} else {
+					System.out.println("you loose the game");
+					stake = stake - 1;
+					System.out.println("After loosing the game your total amount is:" + stake);
+				}
 			}
-			if (startingTotalAmount == 50) {
+			if (stake == 50) {
 				System.out.println("Today you loose your 50% amount so you have to resign for the day");
 			} else {
 				System.out.println("Today you won your 50% amount so you have to resign for the day");
 			}
+			totalAmount = totalAmount + stake;
+			System.out.println(totalAmount);
+		}
+		System.out.println("After 20 days of playing total amount is: " + totalAmount);
+		if (totalAmount > 2000) {
+			int profit = totalAmount - 2000;
+			System.out.println("you are in profit by: " + profit);
+		} 
+		else if (totalAmount < 2000) {
+			int loss = 2000 - totalAmount;
+			System.out.println("you are in loss by: " + loss);
+		} 
+		else {
+			System.out.println("you are neither in loss nor in profit: " + totalAmount);
 		}
 	}
 }
