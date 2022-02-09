f197535a69493c05f581c17c202c45c88dc467a5
smrutikapatil <smruti.patil17@gmail.com>
2022-02-09 21:32:32 +0530

    Added- Either won or loose

--- src/com/gambler_problem/GamblerProblem.java
+++ src/com/gambler_problem/GamblerProblem.java
@@ -2,10 +2,17 @@ package com.gambler_problem;
 
 public class GamblerProblem {
 	public static void main(String[] args) {
-		int stake = 100;
-		int bet = 1;
-		System.out.println("Every day stake: " + stake + "$");
-        System.out.println("Each game bet: " + bet + "$");
-
+		int startingTotalAmount = 100;
+		int betsPerGame = 1;
+		double winOrLoose = Math.floor(Math.random() * 10) % 2;
+		if (betsPerGame == winOrLoose) {
+			System.out.println("you won the game");
+			startingTotalAmount = startingTotalAmount + 1;
+			System.out.println("After winning the game your total amount is:" + startingTotalAmount);
+		} else {
+			System.out.println("you loose the game");
+			startingTotalAmount = startingTotalAmount - 1;
+			System.out.println("After loosing the game your total amount is:" + startingTotalAmount);
+		}
 	}
 }
