32178d214eb5b98393c371bc863ccdcc90418d1a
smrutikapatil <smruti.patil17@gmail.com>
2022-02-10 15:10:29 +0530

    Added- Each month days won or lost

--- src/com/gambler_problem/GamblerProblem.java
+++ src/com/gambler_problem/GamblerProblem.java
@@ -1,11 +1,13 @@
 package com.gambler_problem;
 
 public class GamblerProblem {
-	
+
 	public static void main(String[] args) {
 		int stake = 100;
 		int betsPerGame = 1;
 		int totalAmount = 0;
+		int daysLoss = 0;
+		int daysWon = 0;
 		for (int i = 1; i <= 20; i++) {
 			while (stake > 50 && stake < 150) {
 				double winOrLoose = Math.floor(Math.random() * 10) % 2;
@@ -21,23 +23,24 @@ public class GamblerProblem {
 			}
 			if (stake == 50) {
 				System.out.println("Today you loose your 50% amount so you have to resign for the day");
+				daysLoss = daysLoss + 1;
 			} else {
 				System.out.println("Today you won your 50% amount so you have to resign for the day");
+				daysWon = daysWon + 1;
 			}
 			totalAmount = totalAmount + stake;
 			System.out.println(totalAmount);
 		}
-		System.out.println("After 20 days of playing total amount is: " + totalAmount);
+		System.out.println("After 20 days total amount is:" + totalAmount);
 		if (totalAmount > 2000) {
 			int profit = totalAmount - 2000;
 			System.out.println("you are in profit by: " + profit);
-		} 
-		else if (totalAmount < 2000) {
+		} else if (totalAmount < 2000) {
 			int loss = 2000 - totalAmount;
 			System.out.println("you are in loss by: " + loss);
-		} 
-		else {
+		} else {
 			System.out.println("you are neither in loss nor in profit: " + totalAmount);
 		}
+		System.out.println("In a Month " + daysWon + " days won and " + daysLoss + " days lost ");
 	}
 }
