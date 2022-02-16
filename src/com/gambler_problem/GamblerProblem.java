4c5cf9ba06e5dc3bd1c12d2b0f06a276373d3e31
smrutikapatil <smruti.patil17@gmail.com>
2022-02-16 14:58:32 +0530

    Added- Continue playing next month or stop gambling

--- src/com/gambler_problem/GamblerProblem.java
+++ src/com/gambler_problem/GamblerProblem.java
@@ -1,8 +1,9 @@
 package com.gambler_problem;
 
-public class GamblerProblem {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class GamblerProblem {
+	public void gameStart() {
 		int stake = 100;
 		int betsPerGame = 1;
 		int totalAmount = 0;
@@ -14,37 +15,54 @@ public class GamblerProblem {
 				if (betsPerGame == winOrLoose) {
 					System.out.println("you won the game");
 					stake = stake + 1;
-					System.out.println("After winning the game your total amount is : " + stake);
+					System.out.println("After winning the game your total amount is:" + stake);
 				} else {
 					System.out.println("you loose the game");
 					stake = stake - 1;
-					System.out.println("After loosing the game your total amount is : " + stake);
+					System.out.println("After loosing the game your total amount is:" + stake);
 				}
 			}
 			if (stake == 50) {
 				System.out.println("Today you loose your 50% amount so you have to resign for the day");
 				daysLoss = daysLoss + 1;
-				System.out.println("Day " + i + " is a unlucky day as he lost maximum amount Rs : " + stake);
 			} else {
 				System.out.println("Today you won your 50% amount so you have to resign for the day");
 				daysWon = daysWon + 1;
-				System.out.println("Day " + i + " is a lucky day as he won maximum amount Rs : " + stake);
 			}
 			totalAmount = totalAmount + stake;
 			System.out.println(totalAmount);
 		}
-		System.out.println("After 20 days of playing total amount is : " + totalAmount);
+		System.out.println("After 20 days of playing total amount is: " + totalAmount);
 		if (totalAmount > 2000) {
 			int profit = totalAmount - 2000;
-			System.out.println("you are in profit by : " + profit);
-		} 
-		else if (totalAmount < 2000) {
+			System.out.println("you are in profit by: " + profit);
+		} else if (totalAmount < 2000) {
 			int loss = 2000 - totalAmount;
-			System.out.println("you are in loss by : " + loss);
-		}
-		else {
+			System.out.println("you are in loss by: " + loss);
+		} else {
 			System.out.println("you are neither in loss nor in profit: " + totalAmount);
 		}
-		System.out.println("In a Month " + daysWon + " days won and " + daysLoss + " days lost ");
+		System.out.println("In a Month" + daysWon + "days won and" + daysLoss + "days lost");
+	}
+
+	public static void main(String[] args) {
+
+		GamblerProblem gamblerProblem = new GamblerProblem();
+		int i = 0;
+		while (i == 0) {
+			System.out.println("1.would you like to continue playing next month" + "\n" + "2.stop gambling");
+			Scanner sc = new Scanner(System.in);
+			int option = sc.nextInt();
+			switch (option) {
+			case 1:
+				gamblerProblem.gameStart();
+				break;
+			case 2:
+				i = 1;
+				break;
+			default:
+				System.out.println("compile again");
+			}
+		}
 	}
 }
